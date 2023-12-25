b68679437b900549d0da222daf2293c76e5cfd48
Matthew Lee <mhl34@bil.egr.duke.edu>
2023-12-25 08:43:56 +0000

    fixed some formatting for run.py

--- DataProcessor.py
+++ DataProcessor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 class DataProcessor:
-    def __init__(self):
+    def __init__(self, mainDir = ""):
         self.dexcomFormat = "Dexcom_{0}.csv"
         self.accFormat = "ACC_{0}.csv"
         self.foodLogFormat = "Food_Log_{0}.csv"
@@ -11,32 +11,33 @@ class DataProcessor:
         self.edaFormat = "EDA_{0}.csv"
         self.hrFormat = "HR_{0}.csv"
         self.tempFormat = "TEMP_{0}.csv"
+        self.mainDir = mainDir
       
     def loadData(self, samples, fileType):
         data = {}
         if fileType == "dexcom":
             for sample in samples:
-                df = pd.read_csv(sample + "/" + self.dexcomFormat.format(sample))
+                df = pd.read_csv(self.mainDir + sample + "/" + self.dexcomFormat.format(sample))
                 lst = pd.to_numeric(df['Glucose Value (mg/dL)']).to_numpy()
                 # lst = lst[~np.isnan(lst)]
                 data[sample] = lst
         elif fileType == "temp":
             for sample in samples:
-                df = pd.read_csv(sample + "/" + self.tempFormat.format(sample))
+                df = pd.read_csv(self.mainDir + sample + "/" + self.tempFormat.format(sample))
                 lst = pd.to_numeric(df[' temp']).to_numpy()
                 lst = lst.astype(np.int64)
                 # lst = lst[~np.isnan(lst)]
                 data[sample] = lst
         elif fileType == "eda":
             for sample in samples:
-                df = pd.read_csv(sample + "/" + self.edaFormat.format(sample))
+                df = pd.read_csv(self.mainDir + sample + "/" + self.edaFormat.format(sample))
                 lst = pd.to_numeric(df[' eda']).to_numpy()
                 lst = (lst * 10).astype(np.int64)
                 # lst = lst[~np.isnan(lst)]
                 data[sample] = lst
         elif fileType == "hr":
             for sample in samples:
-                df = pd.read_csv(sample + "/" + self.hrFormat.format(sample))
+                df = pd.read_csv(self.mainDir + sample + "/" + self.hrFormat.format(sample))
                 lst = pd.to_numeric(df[' hr']).to_numpy()
                 lst = lst.astype(np.int64)
                 # lst = lst[~np.isnan(lst)]
--- run.py
+++ run.py
@@ -15,19 +15,21 @@ from PositionalEncoding import PositionalEncoding
 from hyperParams import hyperParams
 from DataProcessor import DataProcessor
 from pp5 import pp5
+from Transformer import Transformer
 from utils import createPersSamples
 
 class runModel:
     def __init__(self, mainDir):
         self.mainDir = mainDir
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     def run(self):
         # load in classes
-        dataProcessor = DataProcessor()
+        dataProcessor = DataProcessor(self.mainDir)
         pp5vals = pp5()
 
         # get eda, temp, glucose, and hr data of all the samples
-        samples = [self.mainDir + str(i).zfill(3) for i in range(1, 17)]
+        samples = [str(i).zfill(3) for i in range(1, 17)]
         glucoseData = dataProcessor.loadData(samples, "dexcom")
         edaData = dataProcessor.loadData(samples, "eda")
         tempData = dataProcessor.loadData(samples, "temp")
@@ -43,7 +45,6 @@ class runModel:
         train_dataloader = self.createBatches(train_data)
         test_dataloader = self.createBatches(test_data)
 
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         hyperparams = hyperParams()
         model = Transformer(
             num_tokens=hyperparams.NUM_TOKENS,
@@ -54,11 +55,11 @@ class runModel:
             num_decoder_layers=hyperparams.NUM_DECODER_LAYERS,
             dropout_p=hyperparams.DROPOUT_P,
             norm_first=hyperparams.NORM_FIRST,
-            device = device
-        ).to(device)
+            device = self.device
+        ).to(self.device)
         opt = optim.Adam(model.parameters(), lr=hyperparams.LEARNING_RATE, eps=1e-8)
         loss_fn = nn.MSELoss()
-        loss_fn = loss_fn.to(device)
+        loss_fn = loss_fn.to(self.device)
         CHECKPOINT_FOLDER = "./saved_model"
 
         train_loss_list, validation_loss_list = self.fit(model, opt, loss_fn, train_dataloader, test_dataloader, 20)
@@ -84,7 +85,7 @@ class runModel:
             X, y = batch[:, 0], batch[:, 1]
             X = [torch.tensor(element) for element in X]
             y = [torch.tensor(element) for element in y]
-            X, y = torch.stack(X).to(device), torch.stack(y).to(device)
+            X, y = torch.stack(X).to(self.device), torch.stack(y).to(self.device)
             # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
             # y_input [SOS_token, ...]
             y_input = y[:-1]
@@ -93,7 +94,7 @@ class runModel:
 
             # Get mask to mask out the next words
             sequence_length = len(y_input)
-            tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+            tgt_mask = model.get_tgt_mask(sequence_length).to(self.device)
             pred = model(X, y_input, tgt_mask)
 
             # Permute pred to have batch size first again
@@ -102,7 +103,7 @@ class runModel:
             y_pred = torch.argmax(pred.detach(), axis=1)
             # print(y_pred, y_expected)
             # print(torch.argmax(pred.detach(), axis=1))
-            mape = mean_absolute_percentage_error(y_expected, y_pred)
+            mape = self.mean_absolute_percentage_error(y_expected, y_pred)
             accuracy.append(100 - mape.detach().item())
             # accuracy.append(comp_accuracy(y_pred, y_expected).item())
             loss = loss_fn(pred, y_expected)
@@ -131,7 +132,7 @@ class runModel:
                 X, y = batch[:, 0], batch[:, 1]
                 X = [torch.tensor(element) for element in X]
                 y = [torch.tensor(element) for element in y]
-                X, y = torch.stack(X).to(device), torch.stack(y).to(device)
+                X, y = torch.stack(X).to(self.device), torch.stack(y).to(self.device)
                 # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
                 # y_input [SOS_token, ...]
                 y_input = y[:-1]
@@ -140,14 +141,14 @@ class runModel:
 
                 # Get mask to mask out the next words
                 sequence_length = len(y_input)
-                tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+                tgt_mask = model.get_tgt_mask(sequence_length).to(self.device)
                 pred = model(X, y_input, tgt_mask)
 
                 # Permute pred to have batch size first again
                 pred = pred.permute(1, 2, 0)
                 # argmax gives the prediction
                 y_pred = torch.argmax(pred.detach(), axis=1)
-                mape = mean_absolute_percentage_error(y_expected, y_pred)
+                mape = self.mean_absolute_percentage_error(y_expected, y_pred)
                 accuracy.append(100 - mape.cpu().detach().item())
                 # accuracy.append(comp_accuracy(y_pred, y_expected).item())
                 loss = loss_fn(pred, y_expected)
